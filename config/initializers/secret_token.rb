b6f428753ea5bf1229ac8474723a28df61310f1b
troythewar <troythewar24@gmail.com>
2014-04-24 12:18:12 +0530

    Designing Ecom Site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecom::Application.config.secret_token = 'fee62c43d5ea9ced7228e2ca0ba9d3426f8d99e216bf3f58b931f83b57176454486337635be1934179e851303d4770fd7b74a0d7ca8f6b6034211444d5ccb0cf'
